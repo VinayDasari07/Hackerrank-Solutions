3510897e21ca58245cfeb3859610cebdae6d70dd
Vinay Dasari <vinay.dasari.07@gmail.com>
2019-06-07 21:08:19 +0530

    Update Strings: Making Anagrams.c

--- Strings/Strings: Making Anagrams.c
+++ Strings/Strings: Making Anagrams.c
@@ -49,7 +49,56 @@ int main()
       }
      
   printf("%d\n",(l1-c)+(l2-c));//addition of the differences of len and common str len will be our ans
-
-
-
 }
+
+-----------------------------------------OPTIMIZED CODE-------------------------------------------------
+  //INSTEAD OF  BIG O(LENGTH OF FIRST STRING * LENGTH OF SECOND STING)
+  //OPTIMIZED CODE IS OF BIG O(LENGTH OF ONE STRING(max length))
+  
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  char str1[10001];
+  char str2[10001];
+ scanf("%[^\n]%*c",str1);
+ scanf("%[^\n]%*c", str2);
+ int l1 = strlen(str1);
+ int l2 = strlen(str2);
+ int i, j, c = 0;
+ int count1[26] = {0};
+ int count2[26] = {0};
+ for (i = 0; i < l1; i++)
+     count1[str1[i]-'a']++; // count the no of occurences of each char of string 1
+   
+ for (i = 0; i < l2; i++)
+     count2[str2[i]-'a']++; // count the no of occurences of each char of string 2
+   
+ for (i = 0; i < 26; i++)
+     c += abs(count1[i]-count2[i]); // add the difference of count of each char in both string
+                                    // for e.g abcde and bccde : here a and one c should be deleted to make them anagrams
+
+  printf("%d\n",c);
+ 
+}
+
+  
+  
+  
